d1c2a6a354023f9fce43a3f37da31c90281f3870
AdoniaJ <jebessah@mail.uc.edu>
2021-08-09 12:26:52 -0500

    hopefully newType and switch working

--- CS3003Summer2021FInalProject/hello.cpp
+++ CS3003Summer2021FInalProject/hello.cpp
@@ -2,6 +2,7 @@
 // two comment lines
 int main() {
     int x;
-
+    big j;
+    j=56;
     switch ('A')  x = 86;
 } // main
--- CS3003Summer2021FInalProject/src/clite/AbstractSyntax.java
+++ CS3003Summer2021FInalProject/src/clite/AbstractSyntax.java
@@ -265,6 +265,7 @@ class Type {
     final static Type CHAR = new Type("char");
     final static Type FLOAT = new Type("float");
     final static Type VOID = new Type("void");
+    final static Type BIG = new Type("big");
     // final static Type UNDEFINED = new Type("undef");
     
     private String id;
@@ -275,7 +276,7 @@ class Type {
 
     // returns the equivaled Jasmin type descriptor
     public String to_jasmin() {
-	if(this.equals(Type.INT) || this.equals(Type.BOOL) || this.equals(Type.CHAR))
+	if(this.equals(Type.INT) || this.equals(Type.BOOL) || this.equals(Type.CHAR)||this.equals(Type.BIG))
 		return "I";
 	else if (this.equals(Type.FLOAT))
 		return "F";
@@ -511,6 +512,11 @@ abstract class Value extends Expression {
         assert false : "should never reach here";
         return 0;
     }
+
+    long bigValue ( ) {
+        assert false : "should never reach here";
+        return 0;
+    }
     
     boolean boolValue ( ) {
         assert false : "should never reach here";
@@ -536,6 +542,7 @@ abstract class Value extends Expression {
         if (type == Type.BOOL) return new BoolValue( );
         if (type == Type.CHAR) return new CharValue( );
         if (type == Type.FLOAT) return new FloatValue( );
+        if (type == Type.BIG) return new BigValue( );
         throw new IllegalArgumentException("Illegal type in mkValue");
     }
 }
@@ -625,7 +632,29 @@ class FloatValue extends Value {
     }
 
 }
+class BigValue extends Value {
+    private long value = 0;
 
+    BigValue ( ) { type = Type.BIG; }
+
+    BigValue (long v) { this( ); value = v; undef = false; }
+
+    long BigValue ( ) {
+        assert !undef : "reference to undefined int value";
+        return value;
+    }
+
+    public String toString( ) {
+        if (undef)  return "undef";
+        return "" + value;
+    }
+
+    public boolean equals(Object obj) {
+    	BigValue iv = (BigValue) obj;
+	return iv.value == this.value;
+    }
+
+}
 class Binary extends Expression {
 // Binary = Operator op; Expression term1, term2
     Operator op;
@@ -690,10 +719,11 @@ class Operator {
     // UnaryOp = !    
     final static String NOT = "!";
     final static String NEG = "-NEG"; //This has been changed from "-" to "-NEG" to avoid ambiguity with MINUS
-    // CastOp = int | float | char
+    // CastOp = int | float | char | big
     final static String INT = "int";
     final static String FLOAT = "float";
     final static String CHAR = "char";
+    final static String BIG = "big";
     // Typed Operators
     // RelationalOp = < | <= | == | != | >= | >
     final static String INT_LT = "INT<";
@@ -702,6 +732,7 @@ class Operator {
     final static String INT_NE = "INT!=";
     final static String INT_GT = "INT>";
     final static String INT_GE = "INT>=";
+    
     // ArithmeticOp = + | - | * | /
     final static String INT_PLUS = "INT+";
     final static String INT_MINUS = "INT-";
@@ -737,11 +768,24 @@ class Operator {
     final static String BOOL_NE = "BOOL!=";
     final static String BOOL_GT = "BOOL>";
     final static String BOOL_GE = "BOOL>=";
+    // RelationalOp = < | <= | == | != | >= | >
+    final static String BIG_LT = "BIG<";
+    final static String BIG_LE = "BIG<=";
+    final static String BIG_EQ = "BIG==";
+    final static String BIG_NE = "BIG!=";
+    final static String BIG_GT = "BIG>";
+    final static String BIG_GE = "BIG>=";
+    // ArithmeticOp = + | - | * | /
+    final static String BIG_PLUS = "BIG+";
+    final static String BIG_MINUS = "BIG-";
+    final static String BIG_TIMES = "BIG*";
+    final static String BIG_DIV = "BIG/";
     // Type specific cast
     final static String I2F = "I2F";
     final static String F2I = "F2I";
     final static String C2I = "C2I";
     final static String I2C = "I2C";
+    
     
     String val;
     
